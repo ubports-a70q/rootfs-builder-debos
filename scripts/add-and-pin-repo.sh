5f9a37f5939004c8291965541ae2495e63cadc34
Ratchanan Srirattanamet <ratchanan@ubports.com>
2020-10-30 02:07:26 +0700

    add-and-pin-repo: accept optional URI part
    This can be used to add a repo from repo2 server or some external
    server, although if the repo is not from UBports, pinning might not
    work.

--- scripts/add-and-pin-repo.sh
+++ scripts/add-and-pin-repo.sh
@@ -7,14 +7,25 @@ echo "nameserver 1.1.1.1" > /etc/resolv.conf
 export DEBIAN_FRONTEND=noninteractive
 export DEBCONF_NONINTERACTIVE_SEEN=true
 
-REPO=$1
-PRI=$2
+if [ "$#" = 3 ]; then
+    URI=$1
+    REPO=$2
+    PRI=$3
+elif [ "$#" = 2 ]; then
+    URI="http://repo.ubports.com/"
+    REPO=$1
+    PRI=$2
+else
+    echo "Usage: add-and-pin-repo.sh [URI] <repo> <priority>"
+fi
 
-echo "deb http://repo.ubports.com/ $REPO main" >> /etc/apt/sources.list.d/ubports.list
+echo "deb $URI $REPO main" >> /etc/apt/sources.list.d/ubports.list
+
+origin=$(echo "$URI"|cut -d '/' -f 3) # Assume well-formed URI.
 
 echo "" >> /etc/apt/preferences.d/ubports.pref
 echo "Package: *" >> /etc/apt/preferences.d/ubports.pref
-echo "Pin: origin repo.ubports.com" >> /etc/apt/preferences.d/ubports.pref
+echo "Pin: origin $origin" >> /etc/apt/preferences.d/ubports.pref
 echo "Pin: release o=UBports,a=$REPO" >> /etc/apt/preferences.d/ubports.pref
 echo "Pin-Priority: $PRI" >> /etc/apt/preferences.d/ubports.pref
 
