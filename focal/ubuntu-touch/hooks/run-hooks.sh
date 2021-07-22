015ee6b899649600e84e89180d4cdb59a7ab8883
Ratchanan Srirattanamet <ratchanan@ubports.com>
2021-07-22 23:44:16 +0700

    focal/ubuntu-touch: add an ability to choose hook type to run
    This allows supporting chroot_early hooks.

--- focal/ubuntu-touch/hooks/run-hooks.sh
+++ focal/ubuntu-touch/hooks/run-hooks.sh
@@ -2,4 +2,12 @@
 
 dir=$(dirname "$0")
 
-exec run-parts --report --exit-on-error --regex='^[0-9]{2}-.*' -- "$dir"
+case "$1" in
+    chroot_early|chroot)
+        ;;
+    *)
+        echo "Invalid hook type $1." >&2
+        exit 1
+esac
+
+exec run-parts --verbose --exit-on-error --regex="^[0-9]{2}-.*\.$1\$" -- "$dir"
